103b7d22379a5a62d1e222f82cccd671679d5442
mani <26954167+webj2ee@users.noreply.github.com>
2022-07-22 15:28:05 +0530

    Update checkoutRepository.groovy
    script changes

--- vars/checkoutRepository.groovy
+++ vars/checkoutRepository.groovy
@@ -1,9 +1,13 @@
 def call(commerceDir, branch, projectRepository) {
     urlPrefix = "https://"
+	repoDomainPart = projectRepository.substring(urlPrefix.size())
+    repository = "https://$USERNAME:$PASSWORD@" + repoDomainPart
+	echo "##### Checkout repository ${repository} #####"
+    
     withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'githubCodeRepoCredentials', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {        
         repoDomainPart = projectRepository.substring(urlPrefix.size())
         repository = "https://$USERNAME:$PASSWORD@" + repoDomainPart
         echo "##### Checkout repository #####"
-        sh """cd ${commerceDir} && git clone ${repository} . && git fetch --all && git checkout origin/${branch}"""
+        bat """cd ${commerceDir} && git clone ${repository} . && git fetch --all && git checkout origin/${branch}"""
     }
 }
