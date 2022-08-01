e2a4469d59fc663b5c5b5ffab2ba663f8e806b74
mani <26954167+webj2ee@users.noreply.github.com>
2022-08-01 17:10:02 +0530

    Update executeCommerceBuild.groovy
    changed in commerce build script

--- vars/executeCommerceBuild.groovy
+++ vars/executeCommerceBuild.groovy
@@ -1,6 +1,7 @@
 def call(commerceDir) {
 	echo "##### Execute install script ##### -- ${commerceDir}"
-	addProperty(commerceDir, "solrserver.instances.default.autostart=false")
-	sh "cd ${commerceDir}/core-customize/hybris/bin/platform && . ./setantenv.sh && ant clean all"
+	//addProperty(commerceDir, "solrserver.instances.default.autostart=false")
+	bat "cd ${commerceDir}/core-customize/hybris/bin/platform && setantenv.sh && ant clean all"
+	//bat "cd ${commerceDir}/hybris/bin/platform && setantenv.bat && hybrisserver.bat"
 } 
    
