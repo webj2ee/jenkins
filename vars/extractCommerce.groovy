d2831fba4511f3aae1f519c26edec2d435093c31
mani <26954167+webj2ee@users.noreply.github.com>
2022-08-01 17:01:02 +0530

    Update extractCommerce.groovy

--- vars/extractCommerce.groovy
+++ vars/extractCommerce.groovy
@@ -1,8 +1,9 @@
 def call(commerceDir) {
     echo "##### Extract commerce platform ##### -- ${commerceDir}"
-    sh "unzip -o ../CXCOM2005*.ZIP -d ${commerceDir}/core-customize"
+    //bat "unzip -o ../CXCOM2005*.ZIP -d ${commerceDir}/core-customize"
     /** Uncomment if you will be using the Integration Extension Pack
     echo "##### Extract commerce integration pack #####"
     sh "unzip -o ../CXCOMINTPK2005*.ZIP -d ${commerceDir}/core-customize"
     **/
+    bat "tar -xvf ${workspace}/CXCOM200500P_0-70004955.zip  -C ${commerceDir}"
 }  
