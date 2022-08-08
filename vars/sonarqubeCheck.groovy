dfc27b802d96ca61561df5b38599a98e04160df3
mani <26954167+webj2ee@users.noreply.github.com>
2022-08-08 16:57:41 +0530

    Update sonarqubeCheck.groovy

--- vars/sonarqubeCheck.groovy
+++ vars/sonarqubeCheck.groovy
@@ -15,10 +15,10 @@ def call(branch, commerceDir, projectName, sonarUrl) {
 
     script {
         SONAR_CHECK_URL = "$sonarUrl/api/qualitygates/project_status?projectKey=$projectName:$BRANCH_WITH_DASH"
-        output = sh(returnStdout: true, script: "echo \$(curl $SONAR_CHECK_URL | jq '.projectStatus.status')")
+        output = bat(returnStdout: true, script: "echo \$(curl $SONAR_CHECK_URL | jq '.projectStatus.status')")
         while (output.contains("NONE")) {
-          sh('sleep 10s')
-          output = sh(returnStdout: true, script: "echo \$(curl $SONAR_CHECK_URL | jq '.projectStatus.status')")
+          sleep(10)
+          output = bat(returnStdout: true, script: "echo \$(curl $SONAR_CHECK_URL | jq '.projectStatus.status')")
         }
         if (output.contains("ERROR")) {
             currentBuild.result = 'UNSTABLE'
