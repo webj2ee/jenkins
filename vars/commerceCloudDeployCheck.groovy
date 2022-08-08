78240a341c0e0213ef53f9a55fe0df8b085cb045
mani <26954167+webj2ee@users.noreply.github.com>
2022-08-08 16:55:10 +0530

    Update commerceCloudDeployCheck.groovy

--- vars/commerceCloudDeployCheck.groovy
+++ vars/commerceCloudDeployCheck.groovy
@@ -2,7 +2,7 @@ def call(deployCode) {
     script {
         while (true) {
           wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${token}", var: 'PASSWD']]]) {
-              result = sh (script: "curl --location --request GET 'https://portalrotapi.hana.ondemand.com/v2/subscriptions/${subscriptionId}/deployments/$deployCode' --header 'Authorization: Bearer ${token}'",returnStdout:true)
+              result = bat (script: "curl --location --request GET 'https://portalrotapi.hana.ondemand.com/v2/subscriptions/${subscriptionId}/deployments/$deployCode' --header 'Authorization: Bearer ${token}'",returnStdout:true)
           }
           echo "$result"
           statusResult = readJSON text: "$result"
@@ -15,7 +15,7 @@ def call(deployCode) {
             error("Deployment was not completed successfully on SAP Commerce Cloud")
           }
 
-          sh('sleep 120s')
+          //sh('sleep 120s')
 
         }
 
