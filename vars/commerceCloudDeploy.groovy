e56f3ef33565695148d422706609a57eaa48095b
mani <26954167+webj2ee@users.noreply.github.com>
2022-08-08 16:54:09 +0530

    Update commerceCloudDeploy.groovy

--- vars/commerceCloudDeploy.groovy
+++ vars/commerceCloudDeploy.groovy
@@ -3,7 +3,7 @@ def call(buildName, dbUpdateMode, environmentId, strategy) {
     //deploy tag 
     script{
         withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'commerceCloudCredentials', usernameVariable: 'subscriptionId', passwordVariable: 'token']]) {
-            deploy = sh (script: "curl --location --request POST 'https://portalrotapi.hana.ondemand.com/v2/subscriptions/${subscriptionId}/deployments' --header 'Content-Type: application/json' --header 'Authorization: Bearer ${token}' --header 'Content-Type: text/plain' --data-raw '{\"buildCode\": \"${buildName}\",\"databaseUpdateMode\": \"${dbUpdateMode}\",\"environmentCode\": \"${environmentId}\",\"strategy\": \"${strategy}\"}'",returnStdout:true)
+            deploy = bat (script: "curl --location --request POST 'https://portalrotapi.hana.ondemand.com/v2/subscriptions/${subscriptionId}/deployments' --header 'Content-Type: application/json' --header 'Authorization: Bearer ${token}' --header 'Content-Type: text/plain' --data-raw '{\"buildCode\": \"${buildName}\",\"databaseUpdateMode\": \"${dbUpdateMode}\",\"environmentCode\": \"${environmentId}\",\"strategy\": \"${strategy}\"}'",returnStdout:true)
             echo "$deploy"
             deploy_result = readJSON text: "$deploy"
             deploy_code = deploy_result["code"]
