d0a72af8da6f573c843af8f80177c8f43a923065
mani <26954167+webj2ee@users.noreply.github.com>
2022-08-08 16:51:05 +0530

    Update buildCommerceCloud.groovy

--- vars/buildCommerceCloud.groovy
+++ vars/buildCommerceCloud.groovy
@@ -3,7 +3,7 @@ def call(branch, buildName) {
     //deploy tag 
     script{
         withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'commerceCloudCredentials', usernameVariable: 'subscriptionId', passwordVariable: 'token']]) {
-            build = sh (script: "curl --location --request POST 'https://portalrotapi.hana.ondemand.com/v2/subscriptions/${subscriptionId}/builds' --header 'Content-Type: application/json' --header 'Authorization: Bearer ${token}' --header 'Content-Type: text/plain' --data-raw '{\"branch\": \"${branch}\",\"name\": \"${buildName}\"}'",returnStdout:true)
+            build = bat (script: "curl --location --request POST 'https://portalrotapi.hana.ondemand.com/v2/subscriptions/${subscriptionId}/builds' --header 'Content-Type: application/json' --header 'Authorization: Bearer ${token}' --header 'Content-Type: text/plain' --data-raw '{\"branch\": \"${branch}\",\"name\": \"${buildName}\"}'",returnStdout:true)
             echo "$build"
             build_result = readJSON text: "$build"
             code_number = build_result["code"]
