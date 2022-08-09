f704f4443cbb1dd5ca8655d050e40f9f7f0690c0
mani <26954167+webj2ee@users.noreply.github.com>
2022-08-09 19:52:09 +0530

    Update builder.groovy

--- dsl/builder.groovy
+++ dsl/builder.groovy
@@ -24,7 +24,7 @@ class JobParameters {
     static void setLibraryBranchParam(job) {
         job.with {
             parameters {
-                stringParam('LIBRARY_BRANCH', 'master', 'Library branch name')
+                stringParam('LIBRARY_BRANCH', 'main', 'Library branch name')
             }
         }
     }
